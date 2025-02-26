6681dbd94cd853a5c7e410cce947cb5a08b14649
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-02-26 14:39:03 -0300

    fix create_file when file cannot be created
    add error handling to prevent an error when the file cannot be created

--- src/file.c
+++ src/file.c
@@ -4,7 +4,11 @@
 
 void create_file(char *file_name){
     FILE *f = fopen(file_name,"a");
+
+    if (f == NULL) {
+        printf("Error creating file %s\n", file_name);
+        return;
+    }
+
     fclose(f);
 }
-
-
