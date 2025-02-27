cb1e5234f79aafbb21bba18b7bccf4486db48826
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-02-27 15:24:00 -0300

    add functions to handle user input
    this commit adds functions to clear input buffer, format inputs, get user input, and validate string and integer inputs

---
+++ include/input.h
@@ -0,0 +1,10 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+void clear_stdin();
+void format_input(char*);
+void user_input(char*, int, char*, char);
+int validate_input(char*);
+int validate_int_input(char*, int, int);
+
+#endif // INPUT_H
---
+++ src/input.c
@@ -0,0 +1,99 @@
+#include "file.h"
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void clear_stdin() {
+    char c;
+    while ((c=getchar() != '\n' && c != EOF));
+    return;
+}
+
+void format_input(char *input) {
+    unsigned first_alpha_index;
+    unsigned last_char_index;
+    unsigned char first_alpha_found;
+    unsigned char spaces_only;
+
+    first_alpha_found = 0;
+    spaces_only = 1;
+
+    for (unsigned i = 0; input[i] != '\0'; i++) {
+        if (input[i] != ' ') {
+            last_char_index = i;
+            spaces_only = 0;
+
+            if (first_alpha_found == 0) {
+                first_alpha_index = i;
+                first_alpha_found = 1;
+            }
+        }
+    }
+
+    if (spaces_only == 1) {
+        input[0] = '\0';
+
+        return;
+    }
+
+    for (unsigned i = first_alpha_index; i <= last_char_index; i++) {
+        if (i == last_char_index) {
+            input[last_char_index + 1] = '\0';
+        }
+
+        input[i - first_alpha_index] = input[i];
+    }
+
+    if (strchr(input, '\n') == NULL) {
+        clear_stdin();
+    } else {
+        input[strlen(input) - 1] = '\0';
+    }
+
+    return;
+}
+
+void user_input(char *input, int length, char *prompt, char break_line) {
+    printf("%s", prompt);
+
+    if (break_line) {
+        printf("\n");
+    }
+
+    fgets(input, length, stdin);
+
+    format_input(input);
+
+    return;
+}
+
+int validate_input(char *input) {
+    char invalid_input = 0;
+
+    if (input[0] == '\0' || input[0] == ' ') {
+        return 1;
+    }
+
+    return invalid_input;
+}
+
+int validate_int_input(char *input, int min, int max) {
+    char invalid_input = 0;
+
+    invalid_input = validate_input(input);
+
+    for (unsigned i = 0; i < strlen(input); i++) {
+        if (!isdigit(input[i])) {
+            invalid_input = 1;
+            break;
+        }
+    }
+
+    if (atoi(input) > max || atoi(input) < min) {
+        invalid_input = 1;
+    }
+
+    return invalid_input;
+}
